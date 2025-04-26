b9e3ac92558818b891e3717ea4807706e84e256b
曾汉 <coedor01@gmail.com>
2025-04-26 18:14:46 +0800

    feat: 初始化项目

---
+++ Avalonia.SnipFrame/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Avalonia.SnipFrame.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Avalonia.SnipFrame/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.SnipFrame.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
